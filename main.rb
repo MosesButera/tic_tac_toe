af03710963db04f4932b67bf25272bc9e5b94031
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-10-23 11:06:42 -0600

    Add interactive prompts to collect player names and symbols and create Player instances Add user prompts to collect person_1 and person_2 names and their x/o symbols. Also, create Player instances using the provided input.

--- main.rb
+++ main.rb
@@ -7,3 +7,18 @@ referee = Referee.new('Referee')
 updated_rows = People.class_variable
 
 puts 'Game Start :)'
+
+# Get player names and values(either X or O)
+puts 'Welcome to TIC_TAC_TOE'
+puts 'Please enter person_1 name'
+person_1_name = gets.chomp
+puts "Please enter X or O for #{person_1_name}"
+person_1_value = gets.chomp
+puts 'Please enter person_2 name'
+person_2_name = gets.chomp
+puts "Please enter X or O for #{person_2_name}"
+person_2_value = gets.chomp
+
+# create an instance with user provided data:
+person_1_instance = Player.new(person_1_name, person_1_value)
+person_2_instance = Player.new(person_2_name, person_2_value)
