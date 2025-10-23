e6e1249a238ce49fcc64ca18dbf4f796f50c74aa
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-10-23 11:03:32 -0600

    Add requires and initial setup (referee, board, welcome message) Add the require_relative lines so the runner can load People, Player, and Referee classes. Also, Create a Referee instance and grab the shared board from People.class_variable. Finally, Print small startup messages so the script confirms it loaded.

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/people'
+require_relative 'lib/player'
+require_relative 'lib/referee'
+
+# Create an instance of referee object
+referee = Referee.new('Referee')
+updated_rows = People.class_variable
+
+puts 'Game Start :)'
