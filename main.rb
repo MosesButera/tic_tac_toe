4dd911e592feb3c85ed293de7c567220dc104ccd
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-10-23 11:08:48 -0600

    Add game loop skeleton and Player 1/Player 2 move + winner check Add a 9.times loop skeleton for up to 9 turns (full board). Also, prompts Player 1 and Player 2 for a row and column, calls Player#play to update the board, updates updated_rows, and asks the referee to check for a a winner. Finally, breaks the loop early if the Referee returns a non-continue message.

--- main.rb
+++ main.rb
@@ -22,3 +22,32 @@ person_2_value = gets.chomp
 # create an instance with user provided data:
 person_1_instance = Player.new(person_1_name, person_1_value)
 person_2_instance = Player.new(person_2_name, person_2_value)
+
+# loop 9 times since there are 9 possible cells on a 3*3 board
+9.times do
+  # Get player 1 move
+  puts "#{person_1_instance.name} please enter row (0-2)"
+  p1_row = gets.chomp.to_i
+  puts "#{person_1_instance.name} please enter column (0-2)"
+  p1_column = gets.chomp.to_i
+
+  # Update table and rows array/play with player 1 move
+  person_1_instance.play(p1_row, p1_column)
+  updated_rows = People.class_variable
+  decision = referee.check_winner(updated_rows)
+
+  break if decision != 'continue playing. no winner!'
+
+  # Get player 2 move
+  puts "#{person_2_instance.name} please enter row (0-2)"
+  p2_row = gets.chomp.to_i
+  puts "#{person_2_instance.name} please enter column (0-2)"
+  p2_column = gets.chomp.to_i
+
+  # Update table/play with player 2 move
+  person_2_instance.play(p2_row, p2_column)
+
+  decision = referee.check_winner(updated_rows)
+
+  break if decision != 'continue playing. no winner!'
+end
