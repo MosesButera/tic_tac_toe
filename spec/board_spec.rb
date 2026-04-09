f0b63b87f97922f61080535c213843bcc55b2776
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-09 04:24:57 -0600

    refactor(board_spec): use change matchers for #place_move (Red) Improve the precision of the #place_move specs by placing static method calls with RSpec change matchers. This ensures we are verifying the actual state change of the board's grid. - Use .to change to verify the specific cell updates from '.' to 'X' - Use .not_to change to confirm state remains stable on invalid input. - Shift from testing method execution to testing state outcomes.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,10 +16,10 @@ describe Board do
     let(:place_move) { instance_double }
     context 'Updates the specific spot on the board with the players marker (X or O)' do
       it 'The updated the cell at the given spot now contains the players marker' do
-        expect(board.place_move(0, 'X'))
+        expect { board.place_move(0, 'X') }.to change { board.grid[0] }.from('.').to('X')
       end
       it 'it does not update the board state if an invalid/out-of-bounds spot is passed' do
-        expect(board.place_move(10, 'O'))
+        expect { board.place_move(99, 'O') }.not_to(change { board.grid })
       end
     end
   end
