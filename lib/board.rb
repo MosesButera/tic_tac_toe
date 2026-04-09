1dfac9c686763c647186086ecca19e3ce3890836
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-09 03:08:38 -0600

    feat(board): implement #display method (Green)
    Write the code to make the Board#display test pass. This method
    converts the internal @board array into a formatted grid for the terminal
    - Use each_slice(3) to group board elements into rows
    - Add separators and padding using lsjust method to create a clean visual layout
    - Fulfils the requirements to output the board state to stdout

--- lib/board.rb
+++ lib/board.rb
@@ -5,5 +5,11 @@ class Board
   attr_reader :board
 
   def display
+    sep = '+----------+---------+'
+    @board.each_slice(3) do |row|
+      puts sep
+      puts "|   #{row.map { |elem| elem.to_s.ljust(2) }.join('  |  ')} |"
+      puts sep
+    end
   end
 end
