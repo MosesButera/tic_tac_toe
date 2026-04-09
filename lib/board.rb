cb71c88e60093fc708cfc1051d49fc284ee38b0e
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-09 02:00:27 -0600

    refactor: update name of from People to Board and initialize @board
    Update name from People to Board class and initialize @board as an Array
    of 9 "." characters instead of a global class variable.
    The Board class will be its own class to discourage high coupling and ensure separation of concerns.
    The class name change to board, reflects a more fitting name for the class.
    Initializing an instance variable @board ensures no other class directly
    modifies the internal state of the board class.

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,5 @@
-class People
-  @@rows = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
-
-  def table(array_rows)
-    sep = '+--------+--------+'
-    puts sep
-    array_rows.map do |r|
-      puts "|  #{r.each { |element| element.to_s.ljust(4) }.join('  |  ')}  |"
-      puts sep
-    end
-  end
-
-  def self.class_variable
-    @@rows
+class Board
+  def initialize
+    @board = Array.new(9, '.')
   end
 end
