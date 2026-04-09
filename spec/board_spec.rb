1454781d839f569ed0e4d60b7f6bc26af7fa9eb9
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-09 08:04:55 -0600

    refactor(board_spec): delete the spec for invalid/out-of-bounds test Delete the entire spec/test for checking that the board state remains unchanged if an invalid spot is passed. This will be handled by the Player class in the choose_move method. This ensures valid input by user right from the beginning.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,9 +18,6 @@ describe Board do
       it 'The updated the cell at the given spot now contains the players marker' do
         expect { board.place_move(0, 'X') }.to change { board.grid[0] }.from('.').to('X')
       end
-      it 'it does not update the board state if an invalid/out-of-bounds spot is passed' do
-        expect { board.place_move(99, 'O') }.not_to(change { board.grid })
-      end
     end
   end
 end
