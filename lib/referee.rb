5e8f085567fed04905466a4ff75cf94c3181f579
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-12 04:03:58 -0600

    fix(referee): fix Referee Class introduction and initialize constant
    Remove class Referee inheritance to non-existent People class and
    only initialize constant WINS for winning all 8 possible win combinations.
    This removes decoupling issues, provides a more robust OOP structure, and ensure class follows single responsibility(SRP).
    - Allow board, player file access using require_relative
    - Delete the  initialization with name as it not required.
    - Delete the check_winner(rows) method implementation as it will be updated based on board and player updated code.

--- lib/referee.rb
+++ lib/referee.rb
@@ -1,32 +1,10 @@
-require_relative 'people'
-require_relative 'player'
-# Create Referee class inheriting from People superclass
-class Referee < People
-  def initialize(name)
-    @name = name
-  end
+require_relative './board'
+require_relative './player'
 
-  def check_winner(rows)
-    # Win cases: There are only 8 possible win cases for either "X" marks or "O" marks.
-    diag_1 = [rows[0][0], rows[1][1], rows[2][2]]
-    diag_2 = [rows[0][2], rows[1][1], rows[2][0]]
-    horiz_1 = [rows[0][0], rows[0][1], rows[0][2]]
-    horiz_2 = [rows[1][0], rows[1][1], rows[1][2]]
-    horiz_3 = [rows[2][0], rows[2][1], rows[2][2]]
-    vert_1 = [rows[0][0], rows[1][0], rows[2][0]]
-    vert_2 = [rows[0][1], rows[1][1], rows[2][1]]
-    vert_3 = [rows[0][2], rows[1][2], rows[2][2]]
-
-    if diag_1.uniq.length == 1 && diag_1.first != '.'
-      p "#{diag_1.first} wins diagonally"
-    elsif diag_2.uniq.length == 1 && diag_2.first != '.'
-      p "#{diag_2.first} wins diagonally"
-    elsif [horiz_1, horiz_2, horiz_3].any? { |r| r.uniq.length == 1 && r.first != '.' }
-      p 'Horizontal winner'
-    elsif [vert_1, vert_2, vert_3].any? { |r| r.uniq.length == 1 && r.first != '.' }
-      p 'Vertical winner'
-    else
-      p 'Continue playing. no winner!'
-    end
-  end
+class Referee
+  WINS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+    [0, 4, 8], [2, 4, 6]             # Diagonals
+  ].freeze
 end
