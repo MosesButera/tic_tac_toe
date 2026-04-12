32398a3150aaa56f95f739df8968212e584d11f3
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-12 03:36:10 -0600

    feat(player): implement #choose_board(move) method (Green)
    Write code to make the Player#choose_board(move) test pass.
    This method prompts player input and returns the input if it is valid.
    (between 1-9 and spot on the board is empty)
    - Use gets to get player input and convert it to zero index value
    - Add a guard clause to ensure we return a valid input
    - Otherwise puts 'Invalid move' and keep prompting for input

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,19 @@
-require_relative 'board'
+require_relative './board'
 
 class Player
   attr_reader :marker
 
   def initialize(marker)
     @marker = marker
+  end
+
+  def choose_move(board)
+    loop do
+      puts "Player #{@marker}, enter 1-9:"
+      input = gets.to_i - 1 # convert to zero index 0-8
+      return input if input.between?(0, 8) && board.is_cell_empty?(input)
+
+      puts 'Invalid move.'
+    end
   end
 end
