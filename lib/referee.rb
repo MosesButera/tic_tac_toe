d03e48f02fd68cd775e59b6a409a47e8efd26582
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-12 05:46:22 -0600

    feat(referee): implement #check_winner(board) method (Green)
    Write code to make the Referee#check_winner(board) test pass.
    This method retrieves the current state of the board in the form of 9 array element,
    iterates through each winning combination and checks if the three positions in the
    current combo are identical and that they are not empty/'.' and returns the marker
    ('X' or 'O') at that winning position if the condition is met.
    If loop ends and condition is not met, it returns nil.

--- lib/referee.rb
+++ lib/referee.rb
@@ -3,8 +3,18 @@ require_relative './player'
 
 class Referee
   WINS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], # Horizontal rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], # Vertical columns
     [0, 4, 8], [2, 4, 6]             # Diagonals
   ].freeze
+
+  def check_winner(board)
+    grid = board.grid
+    WINS.each do |win_combo|
+      if grid[win_combo[0]] == grid[win_combo[1]] && grid[win_combo[1]] == grid[win_combo[2]] && grid[win_combo[0]] != '.'
+        return grid[win_combo[0]] # Return 'X' or 'O'
+      end
+    end
+    nil
+  end
 end
