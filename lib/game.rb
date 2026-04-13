99d1b089e06747095acacf2e2e5aa7cac9117050
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-13 04:44:13 -0600

    feat(game): implement #switch_player method (Green)
    Write the code to make Game#switch_player test pass.
    Use the ternary operator to create a simple decision
    tree that flips the value of an object based on its
    current state. Flip current_player from @p1 at game
    start to @p2 and from @p2 back to @p1 afterwards.
    This will ensure Game can alternate between both
    player objects after each turn and orchestration
    tracks correctly.

--- lib/game.rb
+++ lib/game.rb
@@ -5,11 +5,17 @@ require_relative './player'
 require_relative './referee'
 
 class Game
+  attr_reader :current_player
+
   def initialize
     @board = Board.new
     @p1 = Player.new('X')
     @p2 = Player.new('O')
     @current_player = @p1
     @referee = Referee.new
+  end
+
+  def switch_player
+    @current_player = @current_player == @p1 ? @p2 : @p1
   end
 end
