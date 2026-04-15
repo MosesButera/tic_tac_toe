fc6868513e31e68ba214db04b5cbe357cf0c67ba
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2026-04-15 01:51:22 -0600

    style(board): enhance board display for better user experience
    Improve the 'Board#display' output to make the game
    more interactive and readable in the terminal.
    - Add header and footer for better visual separation
    between turns.
    - Include index 1-9 guide to assist players with coordinate
    selection.
    - Add whitespace (newlines) to prevent terminal text clutter.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Board
   def initialize
     @board = Array.new(9, '.')
@@ -8,12 +10,19 @@ class Board
   end
 
   def display
+    puts "\n--- CURRENT BOARD ---"
+
     sep = '+----------+---------+'
     @board.each_slice(3) do |row|
       puts sep
       puts "|   #{row.map { |elem| elem.to_s.ljust(2) }.join('  |  ')} |"
       puts sep
     end
+
+    puts 'Positions: 1 | 2 | 3'
+    puts '           4 | 5 | 6'
+    puts '           7 | 8 | 9'
+    puts "----------------------\n\n"
   end
 
   def place_move(spot, marker)
